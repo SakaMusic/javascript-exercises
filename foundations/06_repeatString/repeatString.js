54bcda637fa98525868864843883f2e1cb2d740d
SakaMusic <wshbenson@gmail.com>
2026-01-29 20:15:17 -0700

    Completed exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        let strArray = []
+    for (let i = 0; i < num; i++) {
+        strArray.push(str)
+    }
+    return strArray.join("")
+    }
+    
 };
 
 // Do not edit below this line
