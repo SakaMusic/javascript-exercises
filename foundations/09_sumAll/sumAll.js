04971e164c3ebf3f509163786b693969cbdeac8d
SakaMusic <wshbenson@gmail.com>
2026-01-29 21:04:12 -0700

    Completed exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    let total = 0;
+    if (b < a) {
+        for (i = b; i <= a; i++) {
+            total += i;
+        }
+    } else {
+        for (i = a; i <=b; i++) {
+            total += i;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
