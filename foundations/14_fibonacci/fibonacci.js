6d77684dd0fde54dc6ea41bac891234a4851fd70
SakaMusic <wshbenson@gmail.com>
2026-02-02 23:16:48 -0700

    Completed exercise 14 in js-exercises

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    } else if (index == 0) {
+        return 0;
+    }
+    fibArray = [0, 1];
+    let newTerm = 0
+    for (let i = 0; i < index; i++) {
+        newTerm = fibArray[i] + fibArray[i + 1];
+        fibArray.push(newTerm);
+    }
+    return fibArray[index];
 };
 
 // Do not edit below this line
