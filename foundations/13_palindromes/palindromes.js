2ed4c8bffd4c105da5a8cba57f474cdef9dcbf32
SakaMusic <wshbenson@gmail.com>
2026-02-02 22:00:21 -0700

    Completed exercise 13 in js-exercises

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = (string.replace(/[^a-zA-Z0-9]/g, '')).toLowerCase();
+    let neg_i = cleanString.length - 1;
+    for (let i = 0; i < cleanString.length / 2; i++) {
+        if (cleanString[i] === cleanString[neg_i]) {
+            neg_i--;
+        } else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
