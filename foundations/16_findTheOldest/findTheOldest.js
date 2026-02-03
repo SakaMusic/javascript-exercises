00b385f8999fc09f150503a50a84829bb3c36f85
SakaMusic <wshbenson@gmail.com>
2026-02-03 00:20:31 -0700

    Completed exercise 16 in js-exercises

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    function getAge(person) {
+        let age = 0;
+        if (!("yearOfDeath" in person)) {
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        return age;
+    }
+    const currentYear = new Date().getFullYear();
+    return people.reduce((oldestPerson, currentPerson) => {
+        const currentAge = getAge(currentPerson);
+        const oldestAge = getAge(oldestPerson);
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    }, people[0])
 };
 
 // Do not edit below this line
