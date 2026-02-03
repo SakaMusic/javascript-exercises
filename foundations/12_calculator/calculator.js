da1fdb5dff75b729c79d16c15833043d0208dbf1
SakaMusic <wshbenson@gmail.com>
2026-02-02 21:40:47 -0700

    Completed exercise 12 in js-exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	return numArr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  return numArr.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponenet) {
+  let total = 1;
+	for (let i = 0; i < exponenet; i++) {
+    total *= base
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  loopCount = num;
+  for (let i = 0; i < loopCount; i++) {
+    if (num > 0) {
+          total *= num;
+    }
+    num -= 1
+  }
+  return total;
 };
 
 // Do not edit below this line
